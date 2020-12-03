46ac3f30b5e9128293ca1f489850f30b7d4c22b9
Glippy <jonathanong@iMac.local>
2020-12-03 12:37:55 +0800

    3rd screen added

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './screens/HomeScreen';
+import EventsScreen from './screens/EventsScreen';
+import ContactScreen from './screens/ContactScreen';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Event" component={EventsScreen} />
+        <Tab.Screen name="Contact" component={ContactScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/EventsScreen.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+export default function EventScreen(){
+
+            return (
+              <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text>Event ye!</Text>
+              </View>
+            );
+    
+}
